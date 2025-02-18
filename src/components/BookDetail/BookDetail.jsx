6209e1f0d7c8f53556ca17ccd4a0e4f6a6f0b5c8
Rohul14 <aminrohulbd24@gmail.com>
2025-02-19 02:05:40 +0600

    image

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -30,7 +30,7 @@ const BookDetail = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-12 my-6 lg:mb-20 lg:mt-10">
             <div className="p-10 lg:p-20 bg-[#1313130D] rounded-2xl">
-                <img className="lg:h-[564px] lg:w-[425px]" src={image} alt="" />
+                <img className="w-32 h-40 mx-auto lg:h-[564px] lg:w-[425px]" src={image} alt="" />
             </div>
             <div className="flex flex-col gap-6">
                 <div className="flex flex-col gap-4">
